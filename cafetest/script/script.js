827e75425478797d3490872afebd8d1d03ae7e84
Mayday-jayjay <zxcvbnm055218@gmail.com>
2023-11-04 22:28:13 +0800

    新增咖啡1104
    儲存

---
+++ cafetest/script/script.js
@@ -0,0 +1,39 @@
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+
+searchBox.addEventListener("click",()=>{
+    navbar.classList.toggle("showInput")
+    if(navbar.classList.contains("showInput")){
+        searchBox.classList.replace("bx-search" , "bx-x")
+    }else{
+        searchBox.classList.replace("bx-x" , "bx-search")
+    }
+})
+
+
+
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let closeOpenBtn = document.querySelector(".nav-links .bx-x");
+let navLinks = document.querySelector(".nav-links");
+
+menuOpenBtn.addEventListener("click",()=>{
+    navLinks.style.left = "0";
+})
+
+closeOpenBtn.addEventListener("click",()=>{
+    navLinks.style.left = "-100%";
+})
+
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+let jsArrow = document.querySelector(".js-arrow");
+let moreArrow = document.querySelector(".more-arrow");
+
+htmlcssArrow.addEventListener("click",()=>{
+    navLinks.classList.toggle("show1")
+})
+jsArrow.addEventListener("click",()=>{
+    navLinks.classList.toggle("show2")
+})
+moreArrow.addEventListener("click",()=>{
+    navLinks.classList.toggle("show3")
+})
