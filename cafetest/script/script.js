3fea9ab2bbd2b421e4b84700a9720b2fb36186bf
Mayday-jayjay <zxcvbnm055218@gmail.com>
2023-11-12 22:56:20 +0800

    add login and lost-password

--- cafetest/script/script.js
+++ cafetest/script/script.js
@@ -42,3 +42,37 @@ window.addEventListener("scroll",()=>{
     var nav = document.querySelector('nav');
     nav.classList.toggle('sticky', window.scrollY > 0);
 })
+
+// login
+const container = document.querySelector(".container"),
+    pwShowHide = document.querySelectorAll(".showHidePw"),
+    pwFields = document.querySelectorAll(".password"),
+    signUp = document.querySelector(".signup-link"),
+    login = document.querySelector(".login-link");
+    
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click",()=>{
+            pwFields.forEach(pwField=>{
+                if(pwField.type === "password"){
+                    pwField.type = "text";
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("bxs-hide","bxs-show")
+                    })
+                }else{
+                    pwField.type = "password";
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("bxs-show","bxs-hide")
+                    })
+                }
+            })
+        })
+    })
+
+    //change login
+    signUp.addEventListener("click", ()=>{
+        container.classList.add("active");
+    })
+    login.addEventListener("click", ()=>{
+        container.classList.remove("active");
+    })
+
